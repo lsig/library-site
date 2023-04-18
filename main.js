0f09de5f4597925649bf49ac5a2b6884e78b0f07
Lsig <logis21@ru.is>
2023-04-18 12:59:12 +0000

    Simple interface created, logic added to main.js, styles in css

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let library = [];
+let id = 1;
 
 function Book(title, author, length) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.length = length;
@@ -11,3 +13,87 @@ Book.prototype.info = function () {
   const isRead = this.read ? "read" : "not read yet";
   return `${this.title} by ${this.author}, ${this.length} pages, ${isRead} `;
 };
+
+const clearBooks = () => {
+  const books = document.querySelectorAll(".book");
+  books.forEach((book) => {
+    book.remove();
+  });
+};
+
+const addBookToLibrary = () => {
+  clearBooks();
+  const books = document.getElementById("books");
+  library.forEach((book) => {
+    const bookDiv = createBook(book);
+    books.appendChild(bookDiv);
+  });
+};
+
+const createBook = (book) => {
+  const bookDiv = document.createElement("div");
+  bookDiv.setAttribute("id", book.id);
+  bookDiv.setAttribute("class", "book");
+
+  const title = createTitle(book);
+  const author = createAuthor(book);
+  const pages = createPages(book);
+  const button = document.createElement("button");
+  button.innerHTML = "read";
+
+  button.addEventListener("click", (e) => {
+    bookDiv.style.backgroundColor = "#d4e4bc";
+    book.read = true;
+  });
+
+  bookDiv.appendChild(title);
+  bookDiv.appendChild(author);
+  bookDiv.appendChild(pages);
+  bookDiv.appendChild(button);
+
+  return bookDiv;
+};
+
+const createTitle = (book) => {
+  const titleHeader = document.createElement("h2");
+  const titleText = document.createTextNode(book.title);
+  titleHeader.appendChild(titleText);
+  return titleHeader;
+};
+
+const createAuthor = (book) => {
+  const authorHeader = document.createElement("h4");
+  const authorText = document.createTextNode(book.author);
+  authorHeader.appendChild(authorText);
+  return authorHeader;
+};
+
+const createPages = (book) => {
+  const pages = document.createElement("p");
+  const numPages = document.createTextNode(`# pages: ${book.length}`);
+  pages.appendChild(numPages);
+  return pages;
+};
+
+const mockingBird = new Book("To Kill a Mockingbird", "Harper Lee", 300);
+const orwell = new Book("1984", "George Orwell", 302);
+const rye = new Book("The Catcher in the Rye", "J.D. Salinger", 205);
+
+library.push(mockingBird);
+library.push(orwell);
+library.push(rye);
+
+const bookForm = document.getElementById("createBook");
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const bookForm = document.getElementById("createBook");
+  const title = bookForm.elements["title"];
+  const author = bookForm.elements["author"];
+  const pages = bookForm.elements["pages"];
+  const newBook = new Book(title.value, author.value, pages.value);
+  library.push(newBook);
+  id++;
+  addBookToLibrary();
+});
+
+addBookToLibrary();
